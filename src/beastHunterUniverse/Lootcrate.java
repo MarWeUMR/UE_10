b3f23bda9746fb0d61e9f71d82a6ea30d5c97438
CodingAlwaysMessy <marcus.wallau@googlemail.com>
2019-01-17 23:23:42 +0100

    Added Java Doc

--- src/beastHunterUniverse/Lootcrate.java
+++ src/beastHunterUniverse/Lootcrate.java
@@ -3,19 +3,28 @@ package beastHunterUniverse;
 import beastHunterUniverse.weaponTypes.Weapon;
 
 import java.util.ArrayList;
-import java.util.LinkedList;
 import java.util.List;
 
 public class Lootcrate {
 
     private List<Weapon> weaponList;
 
+    /**
+     * Constructor for a Lootcrate instance.
+     *
+     * @param weaponList List of Weapons contained in the crate.
+     */
     public Lootcrate(List<Weapon> weaponList) {
         this.weaponList = weaponList;
-
-
     }
 
+    /**
+     * Method to retrieve all weapons of a given type.
+     *
+     * @param weapon specific weapon object to check against.
+     * @param <T>    allowed types of weapons to check for.
+     * @return list with all weapons of the specified type.
+     */
     public <T extends Weapon> List<T> filter(T weapon) {
 
         List<Weapon> lootList = new ArrayList<>();
@@ -27,7 +36,7 @@ public class Lootcrate {
             }
         }
 
-        return (List<T>)lootList;
+        return (List<T>) lootList;
 
     }
 
